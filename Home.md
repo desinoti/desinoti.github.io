33067c694004b9769c4f5bf8bbfc76d8cc67f718
desinoti <dnt1092@gmail.com>
2022-12-04 21:22:20 +0000

    First

---
+++ Home.md
@@ -0,0 +1,3 @@
+Hello! Welcome to my webpage. 
+
+It's pretty empty at the moment - but please have a look at my resume and my socials
---
+++ base_script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$("#header").load("base/header.html");
+	$("#sidebar").load("base/sidebar.html");
+	$("#footer").load("base/footer.html");
+});
